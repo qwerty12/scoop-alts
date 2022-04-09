1f3e04dfff287bf0bae1bcc2ab17932dfd68e9ea
Faheem Pervez <qwerty12@users.noreply.github.com>
2022-04-09 16:30:38 +0100

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "qwerty12/scoop-alts:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
